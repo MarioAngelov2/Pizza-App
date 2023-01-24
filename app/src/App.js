6c0ce80e4933a4312fed2d11b4dfe868a331c72a
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-01-24 22:37:36 +0200

    Add router and pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+        <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
+ 
--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Logo from "../assets/pizzaLogo.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="leftSide"></div>
-      <div className="rightSide"></div>
+      <div className="leftSide">
+        <img src={Logo} />
+      </div>
+      <div className="rightSide">
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
     </div>
   );
 }
