f074319aef74df964be070ab3e29775fba71f25b
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-01-25 07:13:20 +0200

    Update home page

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div>
-      
+    <div className="home">
+      <div className="headerContainer"></div>
+      <h1>Mario's Pizzaria</h1>
+      <p>PIZZA TO FIT ANY TASTE</p>
+      <Link to="/menu">
+        <button>ORDER NOW</button>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
