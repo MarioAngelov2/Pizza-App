3c5ec6cf15e6e543d192140c8b60bd0c632d5170
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-01-25 21:11:59 +0200

    Add css to home page

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import landing from "../assets/pizza.jpg";
+import "../Styles/Home.css";
 
 function Home() {
   return (
-    <div className="home">
-      <div className="headerContainer"></div>
-      <h1>Mario's Pizzaria</h1>
-      <p>PIZZA TO FIT ANY TASTE</p>
-      <Link to="/menu">
-        <button>ORDER NOW</button>
-      </Link>
+    <div className="home" style={{ backgroundImage: `url(${landing})` }}>
+      <div className="headerContainer">
+        <h1>Mario's Pizzaria</h1>
+        <p>PIZZA TO FIT ANY TASTE</p>
+        <Link to="/menu">
+          <button>ORDER NOW</button>
+        </Link>
+      </div>
     </div>
   );
 }
