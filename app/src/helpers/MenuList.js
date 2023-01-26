ce6a57507c9baf10b8a601451bf84b069f4c9888
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-01-26 11:36:11 +0200

    Create menu list

---
+++ app/src/helpers/MenuList.js
@@ -0,0 +1,33 @@
+import Pepperoni from '../assets/pepperoni.webp';
+import FourCheese from '../assets/fourCheese.jpeg';
+import Vegan from '../assets/vegan.jpeg';
+import Special from '../assets/special.jpeg';
+import Margherita from '../assets/margherita.webp';
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 14.99
+  },
+  {
+    name: "Four Cheese Pizza",
+    image: FourCheese,
+    price: 12.99
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 11.99
+  },
+  {
+    name: "Mario's Special Pizza",
+    image: Special,
+    price: 17.99
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 10.99
+  },
+]
