a552cd94fc9ead88ae77b10a73abbae3da2f5911
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-01-28 17:25:51 +0200

    Create contact page

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/menu" exact element={<Menu />} />
         <Route path="/about" exact element={<About />} />
+        <Route path="/contact" exact element={<Contact />} />
       </Routes>
       <Footer />
     </div>
