c9ccfcae0c5d7128cbb066a9fc975f9a5b424592
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-01-29 18:51:51 +0200

    Update Footer.js

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,7 +1,4 @@
 import React from "react";
-import InstagramIcon from "@material-ui/icons/Instagram";
-import FacebookIcon from "@material-ui/icons/Facebook";
-import TwitterIcon from "@material-ui/icons/Twitter";
 import "../Styles/Footer.css";
 
 function Footer() {
